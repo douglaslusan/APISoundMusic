6d9a36b906ee31d1a58d8cddaa4ebf0ca21d5dac
Douglas Luiz dos Santos <87389167+douglaslusan@users.noreply.github.com>
2023-11-12 18:26:03 -0300

    Primeiro Commit

---
+++ ScreenSound04/ScreenSound04/Filtros/LinqOrder.cs
@@ -0,0 +1,27 @@
+﻿using ScreenSound04.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenSound04.Filtros
+{
+    internal class LinqOrder
+    {
+        public static void OrdenarArtista(List<Musica> musicas)
+        {
+            if (musicas == null)
+            {
+                return;
+            }
+            var artistasOrdenados = musicas.OrderBy(musicas => musicas.Artista).Select(musica => musica.Artista).Distinct().ToList();
+
+            foreach (var artista in artistasOrdenados)
+            {
+                Console.WriteLine(artista);
+            }
+
+        }
+    }
+}
---
+++ ScreenSound04/ScreenSound04/Program.cs
@@ -0,0 +1,29 @@
+﻿using ScreenSound04.Filtros;
+using ScreenSound04.Modelos;
+using System.Text.Json;
+
+using (HttpClient client = new HttpClient())
+{
+    try
+    {
+        string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+        var musicas = JsonSerializer.Deserialize<List<Musica>>(resposta)!;
+
+        // LinqFilter.FiltrarTodosGeneros(musicas);
+
+        // LinqOrder.OrdenarArtista(musicas);
+
+        //LinqFilter.FiltrarArtistaPorGenero(musicas, "pop");
+
+        LinqFilter.FiltrarMusicasPorArtista(musicas, "Eminem");
+
+
+
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+
+    
+}
